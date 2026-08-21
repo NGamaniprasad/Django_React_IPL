44523b3a35176abec7b5f20cf56d6b3168883b71
N Gamini prasad <134373459+NGamaniprasad@users.noreply.github.com>
2026-08-21 17:42:32 +0530

    Update dashboardService.jsx

--- frontend/src/services/dashboardService.jsx
+++ frontend/src/services/dashboardService.jsx
@@ -1,11 +1,59 @@
+// import axios from "axios";
+
+// const API_URL = "http://127.0.0.1:8000/api";
+
+// const getAuthConfig = () => {
+//     const token = localStorage.getItem("access_token");
+    
+    
+
+//     return {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     };
+// };
+
+// const getTeams = () => {
+//     return axios.get(
+//         `${API_URL}/teams/`,
+//         getAuthConfig()
+//     );
+// };
+
+// const getPlayers = () => {
+//     return axios.get(
+//         `${API_URL}/players/`,
+//         getAuthConfig()
+//     );
+// };
+
+// const getTournaments = () => {
+//     return axios.get(
+//         `${API_URL}/tournaments/`,
+//         getAuthConfig()
+//     );
+// };
+
+// const getMatches = () => {
+//     return axios.get(
+//         `${API_URL}/matches/`,
+//         getAuthConfig()
+//     );
+// };
+
+// export default {
+//     getTeams,
+//     getPlayers,
+//     getTournaments,
+//     getMatches,
+// };
 import axios from "axios";
 
 const API_URL = "http://127.0.0.1:8000/api";
 
 const getAuthConfig = () => {
     const token = localStorage.getItem("access_token");
-    
-    
 
     return {
         headers: {
